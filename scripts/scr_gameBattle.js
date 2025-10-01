6590592cb51a2fdaa4b8f8861f5370d87df1bf0a
Wallylll <sousawallysson26@gmail.com>
2025-10-01 18:13:12 -0300

    V1.0

---
+++ scripts/scr_gameBattle.js
@@ -0,0 +1,46 @@
+//Declarando os OBJETOS jogador:
+const player1 = {
+    color: "red",
+    pokemon: "",
+    item1: "",
+    item2: "",
+    item3: ""
+}
+const player2 = {
+    color: "blue",
+    pokemon: "",
+    item1: "",
+    item2: "",
+    item3: ""
+}
+const player3 = {
+    color: "yellow",
+    pokemon: "",
+    item1: "",
+    item2: "",
+    item3: ""
+}
+const player4 = {
+    color: "white",
+    pokemon: "",
+    item1: "",
+    item2: "",
+    item3: ""
+}
+
+//Declarando os OBJETOS Pokemons
+pokemons = [
+    {id: 107, img: "", name: "Hitmonchan", type: "Fighting", hp: 700, atk: "sky_uppercut", spAtk: "detect", def: 70, speed: 50, passiveSkill: "Iron Fist", passiveSkilEffect: "Golpes de soco darão o dobro de dano", atkEffect: "Acerta até pokemons que estão voando lá no alto", spAtkEffect: "Se desvia do golpe detectado", gigantamax: false, megaEvolution: false},
+
+    {id: 0, img: "", name: "", type: "", hp: 0, atk: "", spAtk: "", def: 0, speed: 0, passiveSkill: "", atkEffect: "", spAtkEffect: "", gigantamax: false, megaEvolution: false},
+
+    {id: 0, img: "", name: "", type: "", hp: 0, atk: "", spAtk: "", def: 0, speed: 0, passiveSkill: "", atkEffect: "", spAtkEffect: "", gigantamax: false, megaEvolution: false},
+    
+    {id: 0, img: "", name: "", type: "", hp: 0, atk: "", spAtk: "", def: 0, speed: 0, passiveSkill: "", atkEffect: "", spAtkEffect: "", gigantamax: false, megaEvolution: false}
+];
+
+
+
+
+
+//
