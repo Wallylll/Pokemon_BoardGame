90d79afbb81bd87eacd25aa8f24863bbf4751a2f
FwSenai10 <francisco.w.oliveira10@aluno.senai.br>
2025-10-14 13:40:18 -0300

    V1.4

--- scripts/scr_giveCards.js
+++ scripts/scr_giveCards.js
@@ -1,67 +1,72 @@
 import { players, creatPlayer } from "./scr_jogadores.js";
 
+let red = "oklch(63.7% 0.237 25.331)";
+let whi = "oklch(86.9% 0.022 252.894)"
+let blu = "oklch(62.3% 0.214 259.815)"
+let yel = "oklch(85.2% 0.199 91.936)"
 
-let player1 = creatPlayer("James", "red");
+const btn_playerRed = document.querySelector("#player");
+const playerWhite = document.querySelector("#player");
+const playerBlue = document.querySelector("#player");
+const playerYellow = document.querySelector("#player");
+
+let player1 = creatPlayer("James", "oklch(63.7% 0.237 25.331)");
 players.push(player1);
+console.log(players);
 
-
-let player2 = creatPlayer("Jaison Gay", "Yellow");
-players.push(player2)
-console.log(players)
 /*
-const playerRed = document.querySelector("#playerRed");
-const playerWhite = document.querySelector("#playerWhite");
-const playerBlue = document.querySelector("#playerBlue");
-const playerYellow = document.querySelector("#playerYellow");
+let player2 = creatPlayer("Jaison", "Yellow");
+players.push(player2);
+
+let player3 = creatPlayer("Wally", "Blue");
+players.push(player3);
+
+let player4 = creatPlayer("Lani", "White");
+players.push(player4);
+
 */
+
 const playersCards = document.querySelector("#playersCards");
 
 let container = "";
 
 players.forEach(player => {
     container += `
-            <div class="player" id="player${player.color}">
-                <h2 id="tit_curPlayer">Jogador: ${player.name} </h2>
-
-                <h3>Pokédex: </h3>
-                <div class="pokedex" id="pokedex${player.color}">
-                    ${player.pokedex}
-                </div>
-                <button class="btn_addPokemon">Adicionar Pokémon</button>
-
-                <h3>Itens</h3>
-                <div class="invetory" id="invenory${player.color}">
-                    ${player.inventory}
-                </div>
-                <button class="btn_addItem">Adicionar Item</button>
-            </div>
+        <div class="player" id="player${player.color}">
+        <h2 id="tit_curPlayer">Jogador: ${player.name} </h2>
+        
+        <h3>Pokédex: </h3>
+        <div class="pokedex" id="pokedex${player.color}">
+        ${player.pokedex}
+        </div>
+        <button class="btn_addPokemon id="btn_player${player.color}"">Adicionar Pokémon</button>
+        
+        <h3>Itens</h3>
+        <div class="invetory" id="invenory${player.color}">
+        ${player.inventory}
+        </div>
+        <button class="btn_addItem" id="player${player.color}">Adicionar Item</button>
+        </div>
     `
-    
     playersCards.innerHTML = container;
 });
-//O código não adiciona ao player automaticamente...
-const adicionarPoke = document.querySelectorAll(".btn_addPokemon")
-const addItem = document.querySelectorAll(".btn_addItem")
 
-addItem.forEach(item =>{
+/*
+btn_addPokemon.forEach(pokemon =>{
+    'pokemon'.addEventListener("click", ()=> {
+        player.addPokemon();
+        console.log(player);
+    });
+});
+btn_addItem.forEach(item =>{
     item.addEventListener("click", ()=> {
-        console.log("eu sou um erro ")
+        player.addItem();
+        
+        console.log(player);
     });
-
-})
-adicionarPoke.forEach(pokemon =>{
-    pokemon.addEventListener("click", ()=> {
-        //É preciso especificar o jogador para a função ser executada
-        player1.addPokemon();
-        console.log(player1)
-    });
-})
-
-    
+});
+*/
 
 
-
-
-
-//const cont = document.querySelector("#continue");
-//cont.onclick = ()=> window.location.href = "./gameBattle.html";
+const battle = document.querySelector("#battle");
+battle.onclick = ()=> window.location.href = "./gameBattle.html";
