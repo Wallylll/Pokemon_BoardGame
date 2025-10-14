3384cb2d9fca912641eaa36db5bdff21c6a0039c
FwSenai10 <francisco.w.oliveira10@aluno.senai.br>
2025-10-14 16:31:15 -0300

    V1.4

--- scripts/scr_giveCards.js
+++ scripts/scr_giveCards.js
@@ -1,30 +1,26 @@
 import { players, creatPlayer } from "./scr_jogadores.js";
 
-let red = "oklch(63.7% 0.237 25.331)";
-let whi = "oklch(86.9% 0.022 252.894)"
-let blu = "oklch(62.3% 0.214 259.815)"
-let yel = "oklch(85.2% 0.199 91.936)"
 
+/*
 const btn_playerRed = document.querySelector("#player");
 const playerWhite = document.querySelector("#player");
 const playerBlue = document.querySelector("#player");
 const playerYellow = document.querySelector("#player");
+*/
 
-let player1 = creatPlayer("James", "oklch(63.7% 0.237 25.331)");
+let player1 = creatPlayer("James", "red");
 players.push(player1);
-console.log(players);
-
-/*
 let player2 = creatPlayer("Jaison", "Yellow");
 players.push(player2);
-
 let player3 = creatPlayer("Wally", "Blue");
 players.push(player3);
-
 let player4 = creatPlayer("Lani", "White");
 players.push(player4);
 
-*/
+
+player1.addPokemon();
+
+console.log(players);
 
 const playersCards = document.querySelector("#playersCards");
 
@@ -33,40 +29,58 @@ let container = "";
 players.forEach(player => {
     container += `
         <div class="player" id="player${player.color}">
-        <h2 id="tit_curPlayer">Jogador: ${player.name} </h2>
+            <h2 id="tit_curPlayer">Jogador: ${player.name} </h2>
         
-        <h3>Pokédex: </h3>
-        <div class="pokedex" id="pokedex${player.color}">
-        ${player.pokedex}
-        </div>
-        <button class="btn_addPokemon id="btn_player${player.color}"">Adicionar Pokémon</button>
-        
-        <h3>Itens</h3>
-        <div class="invetory" id="invenory${player.color}">
-        ${player.inventory}
-        </div>
-        <button class="btn_addItem" id="player${player.color}">Adicionar Item</button>
+            <h3>Pokédex: </h3>
+            <div class="pokedex" id="pokedex${player.color}">
+                ${player.pokedex}
+            </div>
+            <button class="btn_addPokemon" id="addPokemon_${player.color}">Adicionar Pokémon</button>
+            
+            <h3>Itens</h3>
+            <div class="invetory" id="invenory${player.color}">
+                ${player.inventory}
+            </div>
+            <button class="btn_addItem">Adicionar Item</button>
         </div>
     `
     playersCards.innerHTML = container;
-});
-
-/*
-btn_addPokemon.forEach(pokemon =>{
-    'pokemon'.addEventListener("click", ()=> {
-        player.addPokemon();
-        console.log(player);
-    });
-});
-btn_addItem.forEach(item =>{
-    item.addEventListener("click", ()=> {
-        player.addItem();
         
-        console.log(player);
-    });
-});
-*/
+    const btn_addPokemon = document.querySelectorAll(".btn_addPokemon");
+    const btn_addItem = document.querySelectorAll(".btn_addItem");
 
+    const containerPlayer = document.querySelectorAll(".player")
+    
+    containerPlayer.forEach(container => {
+        container.addEventListener("click", (evento) => {
+            //console.log(evento.target)
+            
+            if(evento.target.contains(addPokemon_red)){{
+                playerred.addPokemon();
+                }
+                
+                }
+                })
+                })
+                
+                
+                /**
+                 btn_addPokemon.forEach(pokemon =>{
+                    pokemon.addEventListener("click", ()=> {
+                        players[0].addPokemon();
+                        console.log(player);
+                        console.log(players)
+                    });
+                });
+                btn_addItem.forEach(item =>{
+                    item.addEventListener("click", ()=> {
+                        player.addItem();
+                        console.log(player);
+                    });
+                });
+                
+                *  * */
+            });
 
 const battle = document.querySelector("#battle");
 battle.onclick = ()=> window.location.href = "./gameBattle.html";
